96ca4e2024be4f36816ec1333787571ce51a161c
milica-m <4milica@gmail.com>
2019-02-21 13:17:49 +0100

    section about-us done

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+      
+    // EASE SCROLL
+
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+    });
+    
+    
+    //ANIMATION 
+    
+    function animation() {
+        var windowHight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            var position = $(this).offset().top;
+            var animation = $(this).attr('data-animation');
+            //var delay = $(this).attr('data-delay');
+            if (position < scroll + windowHight - 60) {
+                $(this).addClass(animation);
+                //$(this).css('animation-delay', delay);
+            }
+        });
+    }//end of animation function
+
+    animation();
+
+    $(window).scroll(function () {
+        animation();
+    });
+
+});//end document.ready
+
+
